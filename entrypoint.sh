9f28ca936294299b042b7a26a288d141bf0c13b8
Sandro <misfitssandro@gmail.com>
2020-06-16 10:46:14 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo ${GITHUB_REPOSITORY#*/}
+echo ${PWD##*/}
+
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
 	EVENT_ACTION=$(jq -r ".action" "${GITHUB_EVENT_PATH}")
 	if [[ "${EVENT_ACTION}" != "opened" ]]; then
@@ -19,7 +22,7 @@ fi
 sonar-scanner \
 	-Dsonar.host.url=${INPUT_HOST} \
 	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
-	-Dsonar.projectKey=${GITHUB_REPOSITORY} \
+	-Dsonar.projectKey=${GITHUB_REPOSITORY#*/} \
 	-Dsonar.login=${INPUT_LOGIN} \
 	-Dsonar.sources=. \
 	-Dsonar.sourceEncoding=UTF-8
